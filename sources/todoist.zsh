fd982e87e75d5ad063cff2b516218e855b4b3103
byplayer <byplayer100@gmail.com>
2018-10-29 12:28:43 +0800

    add todoist source

---
+++ sources/todoist.zsh
@@ -0,0 +1,31 @@
+# zaw source for todoist
+# https://github.com/sachaos/todoist.git
+
+function zaw-src-todoist-items() {
+  local item_list="$(todoist --header --namespace list)"
+  title="${${(f)item_list}[1]}"
+  items="$(echo $item_list|sed '1d')"
+  id_list="$(echo $items| awk '{print $1}')"
+  : ${(A)candidates::=${(f)id_list}}
+  : ${(A)cand_descriptions::=${(f)items}}
+  actions=(zaw-src-todoist-items-show zaw-src-todoist-items-close zaw-src-todoist-items-delete)
+  act_descriptions=("show" "close" "delete")
+  src_opts=(-t "$title")
+}
+
+function zaw-src-todoist-items-show () {
+  BUFFER="todoist show $1"
+  zle accept-line
+}
+
+function zaw-src-todoist-items-close () {
+  BUFFER="todoist close $1"
+  zle accept-line
+}
+
+function zaw-src-todoist-items-delete () {
+  BUFFER="todoist delete $1"
+  zle accept-line
+}
+
+zaw-register-src -n todoist-items zaw-src-todoist-items
