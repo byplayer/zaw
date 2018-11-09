dae75a5529c5527e5b635677fdd223e847e944f3
byplayer <byplayer100@gmail.com>
2018-11-09 17:24:28 +0800

    rename todoist-items to todoist

--- sources/todoist.zsh
+++ sources/todoist.zsh
@@ -1,31 +1,31 @@
 # zaw source for todoist
 # https://github.com/sachaos/todoist.git
 
-function zaw-src-todoist-items() {
+function zaw-src-todoist() {
   local item_list="$(todoist --header --namespace list)"
   title="${${(f)item_list}[1]}"
   items="$(echo $item_list|sed '1d')"
   id_list="$(echo $items| awk '{print $1}')"
   : ${(A)candidates::=${(f)id_list}}
   : ${(A)cand_descriptions::=${(f)items}}
-  actions=(zaw-src-todoist-items-show zaw-src-todoist-items-close zaw-src-todoist-items-delete)
+  actions=(zaw-src-todoist-show zaw-src-todoist-close zaw-src-todoist-delete)
   act_descriptions=("show" "close" "delete")
   src_opts=(-t "$title")
 }
 
-function zaw-src-todoist-items-show () {
+function zaw-src-todoist-show () {
   BUFFER="todoist show $1"
   zle accept-line
 }
 
-function zaw-src-todoist-items-close () {
+function zaw-src-todoist-close () {
   BUFFER="todoist close $1"
   zle accept-line
 }
 
-function zaw-src-todoist-items-delete () {
+function zaw-src-todoist-delete () {
   BUFFER="todoist delete $1"
   zle accept-line
 }
 
-zaw-register-src -n todoist-items zaw-src-todoist-items
+zaw-register-src -n todoist zaw-src-todoist
